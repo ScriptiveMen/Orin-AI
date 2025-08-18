f2f7da41ba1ffcb68511855db3f93740043b0825
ScriptiveMen <i.satyakumarram@gmail.com>
2025-08-18 19:26:40 +0530

    feat: implement user authentication via JWT in socket server and add message model

--- backend/src/sockets/server.socket.js
+++ backend/src/sockets/server.socket.js
@@ -1,10 +1,68 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+const messageModel = require("../models/message.model");
+const aiService = require("../services/ai.service");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
 
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers.cookie || "");
+
+    if (!cookies) {
+      next(new Error("Please provide token!"));
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+      socket.user = user;
+      next();
+    } catch (error) {
+      next(new Error("Authentication Error : Invalid token"));
+    }
+  });
+
   io.on("connection", (socket) => {
-    console.log("A user is connnected");
+    socket.on("ai-message", async (data) => {
+      await messageModel.create({
+        user: socket.user._id,
+        chat: data.chat,
+        content: data.message,
+        role: "user",
+      });
+
+      const chatHistory = (
+        await messageModel
+          .find({ chat: data.chat })
+          .sort({ createdAt: -1 })
+          .limit(20)
+          .lean()
+      ).reverse();
+
+      const response = await aiService.generateResponse(
+        chatHistory.map((item) => {
+          return {
+            role: item.role,
+            parts: [{ text: item.content }],
+          };
+        })
+      );
+
+      await messageModel.create({
+        user: socket.user._id,
+        chat: data.chat,
+        content: response,
+        role: "model",
+      });
+
+      socket.emit("ai-response", {
+        content: response,
+        chat: data.chat,
+      });
+    });
   });
 }
 
