68ee4c810edec089c6dd3027d7e10e1990d23477
ScriptiveMen <i.satyakumarram@gmail.com>
2025-09-09 00:18:42 +0530

    refactor: remove static file serving and unused route in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,14 +17,9 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
 
 /* Using Routes */
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
-
-app.get("*name", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
 module.exports = app;
